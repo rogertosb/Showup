b40de70363dc23260cb13a70d87a86157c6c618d
xavier auffray <xavierauffray@hotmail.fr>
2020-03-02 19:20:22 +0100

    setting the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :events, only: %i[new create] do
+    resources :tickets, only: %i[new create]
+  end
+  resources :users, only: %i[new create] do
+    resources :tickets, only: %i[new create]
+  end
   root to: 'pages#home'
 end
