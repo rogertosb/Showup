058c6b7ffddf2608d5ed2e299cefa5e856782eb3
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-03-03 13:52:25 +0100

    added landing page link to all events and added the possibility to create and show an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,13 +11,12 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.user_id = current_user
+    @event.user = current_user
     if @event.save
-      redirect_to event_path(event)
+      redirect_to event_path(@event)
     else
       render 'new'
     end
-
   end
 
   def show
