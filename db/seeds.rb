a947a045d6db68b2f22b9e0049ea66513aff26d6
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-03-03 17:10:38 +0100

    we have some sexy seeds now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 # Purges all data before seeding
 
 User.destroy_all
@@ -16,81 +17,33 @@ Ticket.destroy_all
 # Creates 2 users with organizer type
 
 
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "avatar"
-    t.string "organizer_name"
-    t.string "type"
-    t.datetime "created_at", nunamell: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-o1 = User.create(name: "Amstrong", email: "driver1@test.com", password: "hellohello", password_confirmation: "hellohello", photo: "https://i.imgur.com/W8HM214.jpg")
-o2 = User.create(name: "Frimout", email: "driver2@test.com", password: "hellohello", password_confirmation: "hellohello", photo: "https://i.imgur.com/HN3Gmln.jpg")
+o1 = User.create(first_name: nil, last_name: nil, organizer_name: "Le Wagon", user_type: "organizer", email: "lewagon@test.com", password: "hellohello", password_confirmation: "hellohello", avatar: "https://i.imgur.com/W8HM214.jpg")
+o2 = User.create(first_name: nil, last_name: nil, organizer_name: "HIVE Blockchain Society", user_type: "organizer", email: "hive@test.com", password: "hellohello", password_confirmation: "hellohello", avatar: "https://i.imgur.com/HN3Gmln.jpg")
 
 # Creates 4 users with attendee type
 
-a1 = User.create(name: "Max", email: "max@test.com", password: "hellohello", password_confirmation: "hellohello", photo: "https://i.imgur.com/JZaCLJf.jpg")
-a2 = User.create(name: "Xav", email: "xav@test.com", password: "hellohello", password_confirmation: "hellohello", photo: "https://i.imgur.com/jyFGS0K.jpg")
-a3 = User.create(name: "Roger", email: "roger@test.com", password: "hellohello", password_confirmation: "hellohello", photo: "https://i.imgur.com/7mF90bl.jpg")
-a4 = User.create(name: "Gee", email: "gee@test.com", password: "hellohello", password_confirmation: "hellohello", photo: "https://i.imgur.com/zlB6NdR.jpg")
+a1 = User.create(first_name: "Max", last_name: "Hendrickx", organizer_name: nil, user_type: "attendee", email: "max@test.com", password: "hellohello", password_confirmation: "hellohello", avatar: "https://i.imgur.com/JZaCLJf.jpg")
+a2 = User.create(first_name: "Xav", last_name: "Auffray", organizer_name: nil, user_type: "attendee", email: "xav@test.com", password: "hellohello", password_confirmation: "hellohello", avatar: "https://i.imgur.com/jyFGS0K.jpg")
+a3 = User.create(first_name: "Gee", last_name: "Veldekens", organizer_name: nil, user_type: "attendee", email: "roger@test.com", password: "hellohello", password_confirmation: "hellohello", avatar: "https://i.imgur.com/7mF90bl.jpg")
+a4 = User.create(first_name: "Rog", last_name: "Tosbotn", organizer_name: nil, user_type: "attendee", email: "gee@test.com", password: "hellohello", password_confirmation: "hellohello", avatar: "https://i.imgur.com/zlB6NdR.jpg")
 
 
 # Creates events
 
-
-
-  create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "location"
-    t.string "banner"
-    t.string "food"
-    t.string "drink"
-    t.integer "number_max_of_attendees"
-    t.integer "stake"
-    t.date "start_time"
-    t.date "end_time"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
-
-e1 = Event.create(title: "Le Wagon Apero", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", "food": "none", available_seats: 30, spaceship_name: "Moon Express", date: DateTime.parse('2020-03-14T07:00:00'))
-e2 = Event.create(title: "Trollfest", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", "food": "free", available_seats: 200, spaceship_name: "New World", date: DateTime.parse('2020-03-14T07:00:00'))
-e3 = Event.create(title: "Crypto Meeting", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", "food": "paid", available_seats: 10, spaceship_name: "Space Explorer", date: DateTime.parse('2020-03-14T07:00:00'))
-e4 = Event.create(title: "Rugby drink", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", "food": "none", available_seats: 80, spaceship_name: "Rucket", date: DateTime.parse('2020-03-14T07:00:00'))
-e5 = Event.create(title: "Pootsy cleaner get together", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", "food": "none", available_seats: 5, spaceship_name: "Lightspeed", date: DateTime.parse('2020-03-14T07:00:00'))
+e1 = Event.create(user: o1, title: "Le Wagon Apero", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", food: "none", drink: "free", number_max_of_attendees: 30, stake: 5, start_time: DateTime.parse('2020-03-1T07:14:00'), end_time: DateTime.parse('2020-03-1T07:17:00'))
+e2 = Event.create(user: o2, title: "Trollfest", description: "An epic event for trolls", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", food: "free", drink: "free", number_max_of_attendees: 200, stake: 9, start_time: DateTime.parse('2020-03-17T07:12:00'), end_time: DateTime.parse('2020-03-17T07:20:00'))
+e3 = Event.create(user: o2, title: "Crypto Meeting", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", food: "paid", drink: "free", number_max_of_attendees: 10, stake: 5, start_time: DateTime.parse('2020-03-23T07:18:00'), end_time: DateTime.parse('2020-03-23T07:21:00'))
+e4 = Event.create(user: o1, title: "Rugby drink", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", food: "none", drink: "free", number_max_of_attendees: 80, stake: 3, start_time: DateTime.parse('2020-03-14T07:20:00'), end_time: DateTime.parse('2020-03-14T07:23:00'))
+e5 = Event.create(user: o1, title: "Pootsy cleaner get together", description: "An epic event for epic people", location: "Becentral", banner: "https://i.imgur.com/W28v6KK.jpg", food: "none", drink: "free", number_max_of_attendees: 5, stake: 15, start_time: DateTime.parse('2020-04-03T07:18:30'), end_time: DateTime.parse('2020-04-03T07:21:30'))
 
 # Creates reservations
 
-  create_table "tickets", force: :cascade do |t|
-    t.string "status"
-    t.bigint "event_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_tickets_on_event_id"
-    t.index ["user_id"], name: "index_tickets_on_user_id"
 
-Ticket.create(amount_of_passengers: 4, journey: j1, passenger: max)
-Ticket.create(amount_of_passengers: 40, journey: j2, passenger: max)
-Ticket.create(amount_of_passengers: 60, journey: j2, passenger: xav)
-Ticket.create(amount_of_passengers: 20, journey: j2, passenger: roger)
-Ticket.create(amount_of_passengers: 80, journey: j2, passenger: gee)
-Ticket.create(amount_of_passengers: 3, journey: j3, passenger: roger)
-Ticket.create(amount_of_passengers: 1, journey: j3, passenger: roger)
-Ticket.create(amount_of_passengers: 2, journey: j3, passenger: gee)
-Ticket.create(amount_of_passengers: 1, journey: j3, passenger: max)
-Ticket.create(amount_of_passengers: 1, journey: j3, passenger: xav)
-Ticket.create(amount_of_passengers: 1, journey: j3, passenger: xav)
-Ticket.create(amount_of_passengers: 10, journey: j4, passenger: roger)
-Ticket.create(amount_of_passengers: 2, journey: j4, passenger: max)
+t1 = Ticket.new(status: "purchased", event: e1, user: a1)
+t2 = Ticket.new(status: "purchased", event: e1, user: a2)
+t3 = Ticket.new(status: "purchased", event: e3, user: a3)
+t4 = Ticket.new(status: "purchased", event: e4, user: a4)
+t5 = Ticket.new(status: "purchased", event: e2, user: a4)
+t6 = Ticket.new(status: "purchased", event: e5, user: a4)
+t7 = Ticket.new(status: "purchased", event: e1, user: a3)
+t8 = Ticket.new(status: "purchased", event: e2, user: a1)
