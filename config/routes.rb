d4bfacf8df6cebe8749a4a808ba9e0647c29c97d
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-03-03 12:22:26 +0100

    some stuff working some other not

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events, only: %i[new create index] do
+  resources :events do
     resources :tickets, only: %i[new create]
   end
   resources :users, only: %i[new create] do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_154522) do
+ActiveRecord::Schema.define(version: 2020_03_03_111914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 2020_03_02_154522) do
     t.string "drink"
     t.integer "number_max_of_attendees"
     t.integer "stake"
-    t.date "start_time"
-    t.date "end_time"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
