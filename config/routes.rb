0b9c0d5c2af217f904da27ef68b5a7409718800b
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-03-04 15:00:37 +0100

    adding the ticket reservations system

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events do
-    resources :tickets, only: %i[new create]
+    resources :tickets, only: %i[create index]
   end
-  resources :users, only: %i[new create] do
-    resources :tickets, only: %i[new create]
-  end
+
+  resources :tickets, only: %i[edit update show detroy]
+  resources :users, only: %i[new create]
+
   resources :users, only: %i[show edit update] do
     resources :events, only: %i[new create]
   end
-  get 'pages/sign_up_details', to: 'pages#sign_up_details', as: :sign_up_details
 
+  get 'pages/sign_up_details', to: 'pages#sign_up_details', as: :sign_up_details
   get 'dashboard', to: 'dashboards#index', as: :dashboard
   root to: 'pages#home'
 end
