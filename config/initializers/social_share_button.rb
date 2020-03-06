d7c8bcc3cc4b7021ef584e4192031594ccc35790
Roger Tosbotn <rogertosb@gmail.com>
2020-03-06 11:36:21 +0100

    draft social sharing buttons

---
+++ config/initializers/social_share_button.rb
@@ -0,0 +1,4 @@
+SocialShareButton.configure do |config|
+  config.allow_sites = %w(twitter facebook google_plus google_bookmark
+                          pinterest email linkedin reddit telegram)
+end
