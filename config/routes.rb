28a04b5f9953c073a14354cc770f48a940d68e75
xavier auffray <xavierauffray@hotmail.fr>
2020-03-06 16:34:39 +0100

    this time the good version for demo first week

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'users/sign_out', to: 'devise/sessions#destroy'
+  # delete 'users/sign_out', to: 'devise/sessions#destroy'
 
   resources :events do
     resources :tickets, only: %i[create index]
