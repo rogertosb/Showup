0490b398eaf5a1e6b648c0aebd406dbc254bf485
guillaume veldekens <guillaume.veldekens@gmail.com>
2020-03-09 18:31:25 +0100

    changing the image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,9 @@
 import "bootstrap";
 import {initAutocomplete} from '../plugins/autocomplete'
+import $ from "jquery"
 
 initAutocomplete()
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
