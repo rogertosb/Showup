d874a105c9f72792c2e7fb1c4aba075433cc9ea4
xavier auffray <xavierauffray@hotmail.fr>
2020-03-12 14:37:08 +0100

    seed update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,11 +15,12 @@ class OrdersController < ApplicationController
       payment_intent_data: {
         capture_method: "manual"
       },
+      customer_email: current_user.email,
       success_url: order_url(order),
       cancel_url: order_url(order)
     )
 
-    order.update(checkout_session_id: session.id)
+    order.update(checkout_session_id: session.id, payment_intent_id: session.payment_intent)
     redirect_to new_order_payment_path(order)
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_130623) do
+ActiveRecord::Schema.define(version: 2020_03_12_132449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_130623) do
     t.bigint "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "payment_intent_id"
     t.index ["event_id"], name: "index_orders_on_event_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
