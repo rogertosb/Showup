9d59834146798cfb084e193ac79b2be3ac6dad69
Roger Tosbotn <rogertosb@gmail.com>
2020-03-12 11:35:05 +0100

    we have search attendees but it only takes ILIKE queries, so either firstname or lastname needs to be spelled correctly to work. We could later try to modify the controller of tickets to make the search take 'similar to' or 'contains' or something

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :tickets, dependent: :destroy
+  has_many :users, through: :tickets
   has_many :orders, dependent: :destroy
   has_one_attached :photo
 
