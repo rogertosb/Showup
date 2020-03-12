dbd866886f94bb952c0003203823d742f5fdc465
xavier auffray <xavierauffray@hotmail.fr>
2020-03-12 13:23:51 +0100

    styling payment pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
 //= require rails-ujs
 //= require jquery
-//= require social-share-button
-//= require_tree .
-
-//= require jquery
-
 //= require activestorage
 //= require micromodal/dist/micromodal
+//= require_tree .
+
+//= require social-share-button
+
 
